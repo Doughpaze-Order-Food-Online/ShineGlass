c16483d7305a95df362c33d54f0d230bd9539456
siddhantdrk <siddhantdrk@gmail.com>
2020-09-11 05:18:35 +0530

    UI updated

--- app/src/main/java/com/mg/shineglass/MainActivity.java
+++ app/src/main/java/com/mg/shineglass/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_account);
+        setContentView(R.layout.login_signup_activity);
     }
 }
