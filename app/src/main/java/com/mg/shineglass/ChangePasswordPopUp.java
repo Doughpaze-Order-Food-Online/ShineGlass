2425f282edf5cfa6bbe95bdf971143021024a8ca
siddhantdrk <siddhantdrk@gmail.com>
2020-09-26 14:01:45 +0530

    ChangePasswordPopUp.java added progress bar in pop ups added

---
+++ app/src/main/java/com/mg/shineglass/ChangePasswordPopUp.java
@@ -0,0 +1,14 @@
+package com.mg.shineglass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChangePasswordPopUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.change_password_fragment);
+    }
+}
