41ea78d42f51fbb811ffb6b2ddce31bae3098d63
siddhantdrk <siddhantdrk@gmail.com>
2020-10-02 12:48:21 +0530

    App icon updated and back btn added to navigation activities

--- app/src/main/java/com/mg/shineglass/HelpOrContactUs.java
+++ app/src/main/java/com/mg/shineglass/HelpOrContactUs.java
@@ -3,12 +3,18 @@ package com.mg.shineglass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class HelpOrContactUs extends AppCompatActivity {
+
+    private ImageView BackBtnImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_or_contact_us);
+        BackBtnImg=findViewById(R.id.back_btn_img);
+        BackBtnImg.setOnClickListener(view -> finish());
     }
 }
--- app/src/main/java/com/mg/shineglass/TermsAndPolicies.java
+++ app/src/main/java/com/mg/shineglass/TermsAndPolicies.java
@@ -2,13 +2,18 @@ package com.mg.shineglass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class TermsAndPolicies extends AppCompatActivity {
 
+    private ImageView BackBtnImg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terms_and_policies);
+        BackBtnImg=findViewById(R.id.back_btn_img);
+        BackBtnImg.setOnClickListener(view -> finish());
     }
 }
